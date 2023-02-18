e896968d0ada147c440ccced340e6ed1cd7e91ba
Naman0508 <nsadhikari21@gmail.com>
2023-02-18 23:04:24 +0530

    email

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -1,5 +1,7 @@
 package com.driver;
 
+import java.util.HashSet;
+
 public class Email {
 
     private String emailId;
@@ -25,5 +27,29 @@ public class Email {
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
-    }
+        if(oldPassword.equals(password)) {
+            if (isValid(newPassword)) {
+                this.password = newPassword;
+            }
+        }
+        }
+        public boolean isValid(String input){
+            int n = input.length();
+            boolean hasLower = false, hasUpper = false,
+                    hasDigit = false, specialChar = false;
+
+            for (char i : input.toCharArray())
+            {
+                if (Character.isLowerCase(i))
+                    hasLower = true;
+                if (Character.isUpperCase(i))
+                    hasUpper = true;
+                if (Character.isDigit(i))
+                    hasDigit = true;
+                if ((i>='0' && i<='9') && !(i>='a' && i<='z') && !(i>='A' && i<='Z') )
+                    specialChar = true;
+            }
+            if((n>8) && hasLower && hasDigit && hasUpper && specialChar) return true;
+            return false;
+        }
 }
